2c3f359596606c173c60eca39aff12b1984d8bab
websteak <ssk0080@gmail.com>
2022-11-12 17:36:04 +0900

    221112

---
+++ 2211/221112/별_찍기-2.ts
@@ -0,0 +1,24 @@
+/*
+문제: 첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+입력: 첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+출력: 첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+입력1: 5
+출력1: 
+    *
+   **
+  ***
+ ****
+*****
+*/
+
+const input = `5`;
+const inputNum = Number(input);
+
+for (let i = 0; i < inputNum; i++) {
+  let star = '';
+  for (let j = inputNum - 1; j >= 0; j++) {
+    star += j <= i ? '*' : ' ';
+  }
+  console.log(star);
+}
