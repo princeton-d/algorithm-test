ce3b4cb92509b3f2b8f9b13f2296d2a0560137ae
websteak <ssk0080@gmail.com>
2022-12-30 18:07:52 +0900

    4673 셀프 넘버

---
+++ 2212/221230/4673_셀프_넘버/input.js
@@ -0,0 +1,20 @@
+const arr = Array.from({ length: 10001 })
+
+function d(n) {
+  let num = n
+  const splitNum = String(n).split('').map(Number)
+  for (let i = 0; i < splitNum.length; i++) {
+    num += splitNum[i]
+  }
+  return num
+}
+
+for (let i = 1; i <= 10000; i++) {
+  d(i)
+  arr[d(i)] = d(i)
+}
+
+// arr.map(e => e !== undefined ? console.log(e) : '')
+for (let j = 1; j <= 10000; j++) {
+  arr[j] === undefined ? console.log(j) : ''
+}
