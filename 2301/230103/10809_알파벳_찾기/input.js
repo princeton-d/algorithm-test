e268f3930fa37128f1ee43bc8c8d3a30f6c148db
websteak <ssk0080@gmail.com>
2023-01-03 18:15:08 +0900

    10809 알파벳 찾기

---
+++ 2301/230103/10809_알파벳_찾기/input.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const filePath = process.platform === "linux" ? '/dev/stdin' : __dirname + '/input.txt'
+const input = fs.readFileSync(filePath).toString().trim()
+const x = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for (let i = 0; i < x.length; i++) {
+  x[i] = input.indexOf(x[i])
+}
+console.log(x.join(' '))
