56bbb0b434b40bc7a93068c642b238007023ce7d
websteak <ssk0080@gmail.com>
2023-01-19 20:47:54 +0900

    18310 안테나

--- 2301/230119/18310 안테나/input.js
+++ 2301/230119/18310 안테나/input.js
@@ -2,10 +2,7 @@ const fs = require('fs')
 const filePath = process.platform === "linux" ? '/dev/stdin' : __dirname + '/input.txt'
 let [N, H] = fs.readFileSync(filePath).toString().trim().split('\n')
 
+N = N % 2 === 0 ? N / 2 - 1 : N / 2 - 0.5
 H = H.split(' ').map(Number).sort((a, b) => a - b)
 
-if (H.length % 2 === 0) {
-  console.log(H[(H.length / 2) - 1])
-} else {
-  console.log(H[Math.floor(H.length / 2)])
-}
+console.log(H[N])
