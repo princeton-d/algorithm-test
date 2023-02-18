a8543152688f2fe965ddbf16912d435b07db09ff
websteak <ssk0080@gmail.com>
2023-02-18 22:19:31 +0900

    10757 큰 수 A+B

--- 2302/230216/10757_큰_수_A+B/input.js
+++ 2302/230216/10757_큰_수_A+B/input.js
@@ -2,4 +2,4 @@ const fs = require('fs')
 const filePath = process.platform === "linux" ? '/dev/stdin' : __dirname + '/input.txt'
 let [x, y] = fs.readFileSync(filePath).toString().trim().split(" ");
 
-console.log(String(BigInt(x) + BigInt(y)))
+console.log(String(BigInt(x) + BigInt(y)));
