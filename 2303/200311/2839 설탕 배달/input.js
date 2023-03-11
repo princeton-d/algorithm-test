6664ad206439eeca592861831ffefe0349ed09c0
websteak <ssk0080@gmail.com>
2023-03-11 18:19:08 +0900

    2839 설탕 배달

---
+++ 2303/200311/2839 설탕 배달/input.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const filePath = process.platform === "linux" ? '/dev/stdin' : __dirname + '/input.txt'
+let I = +fs.readFileSync(filePath).toString().trim();
+let count = 0;
+
+while (true) {
+  if (I % 5 === 0) {
+    console.log(I / 5 + count);
+    break;
+  }
+
+  if (I < 0) {
+    console.log(-1);
+    break;
+  }
+
+  I -= 3;
+  count++;
+}
--- sample/input.js
+++ sample/input.js
@@ -1,6 +1,3 @@
 const fs = require('fs')
 const filePath = process.platform === "linux" ? '/dev/stdin' : __dirname + '/input.txt'
 let I = fs.readFileSync(filePath).toString().trim()
-
-
-// let I = require('fs').readFileSync('/dev/stdin').toString().trim()
