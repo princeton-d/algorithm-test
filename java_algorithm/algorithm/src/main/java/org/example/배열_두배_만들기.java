0c9341d298ea3e4ce902d2f3b2d4c8b39fcfd468
websteak <ssk0080@gmail.com>
2023-04-05 21:34:22 +0900

    프로그래머스 배열 두 배 만들기

---
+++ java_algorithm/algorithm/src/main/java/org/example/배열_두배_만들기.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class 배열_두배_만들기 {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        
+        for (int i = 0; i < numbers.length; i++) {
+            answer[i] = numbers[i] * 2;
+        }
+        
+        return answer;
+    }
+}
