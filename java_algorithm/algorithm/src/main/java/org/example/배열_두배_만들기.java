e2db7b0221d701f5677556d91e6b1c87d8e5f305
websteak <ssk0080@gmail.com>
2023-04-05 21:38:18 +0900

    프로그래머스 배열 두 배 만들기 다른 풀이

--- java_algorithm/algorithm/src/main/java/org/example/배열_두배_만들기.java
+++ java_algorithm/algorithm/src/main/java/org/example/배열_두배_만들기.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class 배열_두배_만들기 {
     public int[] solution(int[] numbers) {
         int[] answer = new int[numbers.length];
@@ -9,5 +11,11 @@ public class 배열_두배_만들기 {
         }
         
         return answer;
+    }
+    
+    public int[] solution2(int[] numbers) {
+        return Arrays.stream(numbers)
+            .map(number -> number * 2)
+            .toArray();
     }
 }
