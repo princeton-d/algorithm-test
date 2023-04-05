fa3f8cfcf2c514af4b75eccb983e7c5c17dece87
websteak <ssk0080@gmail.com>
2023-04-05 23:20:39 +0900

    프로그래머스 문자열 뒤집기

---
+++ java_algorithm/algorithm/src/main/java/org/example/문자열_뒤집기.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class 문자열_뒤집기 {
+    public String solution(String my_string) {
+        StringBuffer sb = new StringBuffer(my_string);
+        String answer = sb.reverse().toString();
+        return answer;
+    }
+}
