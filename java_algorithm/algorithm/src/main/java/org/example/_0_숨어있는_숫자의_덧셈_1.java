acfc8b91593080de6ffb161d5cef0245f4031ac1
websteak <ssk0080@gmail.com>
2023-04-08 17:00:54 +0900

    0 숨어있는 숫자의 덧셈 1

---
+++ java_algorithm/algorithm/src/main/java/org/example/_0_숨어있는_숫자의_덧셈_1.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class _0_숨어있는_숫자의_덧셈_1 {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] stringArr = my_string.replaceAll("[^0-9]", "").split("");
+        for (String number : stringArr) {
+            answer += Integer.parseInt(number);
+        }
+        return answer;
+    }
+}
