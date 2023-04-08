7250bb1d17296ebca230f085e02bc21982265189
websteak <ssk0080@gmail.com>
2023-04-08 17:47:14 +0900

    0 개미 군단

---
+++ java_algorithm/algorithm/src/main/java/org/example/_0_개미_군단.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class _0_개미_군단 {
+    public int solution(int hp) {
+        int count = 0;
+        while (hp != 0) {
+            
+            if (hp >= 5) {
+                count++;
+                hp -= 5;
+            }
+            
+            else if (hp >= 3) {
+                count++;
+                hp -= 3;
+            }
+            
+            else if (hp >= 1) {
+                count++;
+                hp--;
+            }
+        }
+        
+        return count;
+    }
+}
