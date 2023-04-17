a063c47ae54e4d89f9d17f638bda3684277e9745
websteak <ssk0080@gmail.com>
2023-04-17 20:02:57 +0900

    1 짝수와 홀수

---
+++ java_algorithm/algorithm/src/main/java/org/example/_1_짝수와_홀수.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class _1_짝수와_홀수 {
+    class Solution {
+        public String solution(int num) {
+            return num % 2 == 0 ? "Even" : "Odd";
+        }
+    }
+}
