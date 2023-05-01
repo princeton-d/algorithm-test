1a5eaabd3d9c9b90fd05d5b539ee744c2ce9d6d4
princeton <ssk0080@gmail.com>
2023-05-01 17:52:43 +0900

    이노캠 과제 수정

--- java_algorithm/algorithm/src/main/java/org/example/백준_2739_구구단.java
+++ java_algorithm/algorithm/src/main/java/org/example/백준_2739_구구단.java
@@ -6,7 +6,7 @@ public class 백준_2739_구구단 {
         int input = 2;
 
         for (int i = 1; i <= 9; i++) {
-            System.out.println("2 * " + i + " = " + input * i);
+            System.out.println(input + " * " + i + " = " + input * i);
         }
     }
 }
