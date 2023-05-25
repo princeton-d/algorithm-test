1947355634b309b6f8c2fe256fa7a463263af0cb
princeton <ssk0080@gmail.com>
2023-05-25 09:31:41 +0900

    프로그래머스 2 이진 변환 반복하기

---
+++ java_algorithm/algorithm/src/main/java/org/example/_2_이진_변환_반복하기.java
@@ -0,0 +1,32 @@
+package org.example;
+
+public class _2_이진_변환_반복하기 {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        int count = 0;
+        int zeroCount = 0;
+
+        while (s.length() != 1) {
+            int oneCount = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '0') {
+                    zeroCount++;
+                }
+
+                if (s.charAt(i) == '1') {
+                    oneCount++;
+                }
+            }
+
+            s = s.replaceAll("0", "");
+            s = Integer.toBinaryString(oneCount);
+            count++;
+        }
+
+        answer[0] = count;
+        answer[1] = zeroCount;
+
+        return answer;
+    }
+}
