659a999193c9a1fcbde37019aa47bffb3ab79485
princeton <ssk0080@gmail.com>
2023-05-26 00:32:07 +0900

    프로그래머스 2 귤 고르기

--- java_algorithm/algorithm/src/main/java/org/example/_2_귤_고르기.java
+++ java_algorithm/algorithm/src/main/java/org/example/_2_귤_고르기.java
@@ -1,16 +1,8 @@
 package org.example;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
-public class Test {
-    public static void main(String[] args) {
-        int[] arr = new int[]{1, 4, 6, 4, 2, 74, 25, 21, 13};
-        List<Integer> lists = Arrays.stream(arr).sorted().boxed().collect(Collectors.toList());
-        LinkedList<Integer> list = new LinkedList<>(lists);
-        System.out.println(list.getLast());
-    }
-
+public class _2_귤_고르기 {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
         int count = 0;
