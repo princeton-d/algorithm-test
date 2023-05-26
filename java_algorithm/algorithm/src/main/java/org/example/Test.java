b178e026c5ffe534a77d13e71959f3a387f9ae11
princeton <ssk0080@gmail.com>
2023-05-26 13:55:08 +0900

    프로그래머스 n^2 배열 자르기

--- java_algorithm/algorithm/src/main/java/org/example/Test.java
+++ java_algorithm/algorithm/src/main/java/org/example/Test.java
@@ -5,9 +5,32 @@ import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-
+        System.out.println(Arrays.toString(solution(5, 5L, 13L)));
     }
 
+    public static int[] solution(int n, long left, long right) {
+        int leftX = (int) left % n;
+        int leftY = (int) left / n;
+        int rightX = (int) right % n;
+        int rightY = (int) right / n;
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = leftY; i <= rightY; i++) {
+
+            for (int j = leftX; j <= rightX; j++) {
+
+                if (j <= i) {
+                    list.add(i + 1);
+                    continue;
+                }
+
+                list.add(j + 1);
+            }
+
+            leftX = 0;
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
 
 }
