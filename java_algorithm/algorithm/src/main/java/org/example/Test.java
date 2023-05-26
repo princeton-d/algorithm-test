83242f464314686684fd6261489abae1e0306d18
princeton <ssk0080@gmail.com>
2023-05-26 17:31:34 +0900

    백준 s4 동전 0

--- java_algorithm/algorithm/src/main/java/org/example/Test.java
+++ java_algorithm/algorithm/src/main/java/org/example/Test.java
@@ -5,32 +5,15 @@ import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(5, 5L, 13L)));
+        System.out.println(dfs(40));
     }
 
-    public static int[] solution(int n, long left, long right) {
-        int leftX = (int) left % n;
-        int leftY = (int) left / n;
-        int rightX = (int) right % n;
-        int rightY = (int) right / n;
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = leftY; i <= rightY; i++) {
-
-            for (int j = leftX; j <= rightX; j++) {
-
-                if (j <= i) {
-                    list.add(i + 1);
-                    continue;
-                }
-
-                list.add(j + 1);
-            }
-
-            leftX = 0;
+    public static int dfs(int number) {
+        if (number <= 2) {
+            return 1;
         }
 
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return dfs(number - 1) + dfs(number - 2);
     }
 
 }
