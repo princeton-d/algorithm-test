7140e15dcc95c1e49d10878851d65c1f2022997a
princeton <ssk0080@gmail.com>
2023-05-26 11:07:31 +0900

    필요없는 파일 삭제

--- a.js
+++
@@ -1,4 +0,0 @@
-const input = "5 8 4";
-const splitInput = input.split(" ").map((item) => { item = parseInt(item) });
-
-console.log(splitInput)
--- test.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const filePath = process.platform === "linux" ? '/dev/stdin' : __dirname + '/input.txt';
-const input = fs.readFileSync(__dirname + '/input.txt').toString().trim()
-
-// let I = require('fs').readFileSync('/dev/stdin').toString().trim()
