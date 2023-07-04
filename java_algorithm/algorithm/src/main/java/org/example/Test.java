902215debdbb7987478645ab878dfdcb99409af7
princeton <ssk0080@gmail.com>
2023-07-04 20:03:21 +0900

    test

--- java_algorithm/algorithm/src/main/java/org/example/Test.java
+++ java_algorithm/algorithm/src/main/java/org/example/Test.java
@@ -1,19 +1,30 @@
 package org.example;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Test {
     public static void main(String[] args) {
-        int n = 45;
+        int k = 80;
+        int[][] dungeons = {{80, 20}, {50, 40}, {30, 10}};
+        System.out.println(solution(k, dungeons));
+    }
 
-        while (true) {
-            System.out.println(n % 3);
-            System.out.println(n / 3);
+    public static boolean[] visited;
+    public static int answer = -1;
 
-            if (n == 1 || n == 0) {
-                break;
+    public static int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        dfs(k, dungeons, 0);
+        return answer;
+    }
+
+    public static void dfs(int k, int[][] dungeons, int count){
+        answer = Math.max(answer, count);
+        for(int i=0; i<dungeons.length; i++){
+            if(visited[i] == false && dungeons[i][0] <= k){
+                visited[i] = true;
+                dfs(k - dungeons[i][1], dungeons, count+1);
+                visited[i] = false;
             }
         }
     }
