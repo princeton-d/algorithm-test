3ceae22a149e9fc3e00bb807c2217d217d8f1a45
kimdonghyun <ssk0080@gmail.com>
2023-09-30 00:00:11 +0900

    백준 b5 A + B 2

---
+++ java_algorithm/algorithm/src/main/java/org/example/baekjoon/bronze/AB2.java
@@ -0,0 +1,24 @@
+package org.example.baekjoon.bronze;
+
+import java.io.*;
+
+public class AB2 {
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/main/java/org/example/baekjoon/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        sb.append(A + B);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+
+}
