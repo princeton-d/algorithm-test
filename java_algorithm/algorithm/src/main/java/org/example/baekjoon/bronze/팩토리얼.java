20abd9f7023613b7fd40a2f898aeeb82605e5d28
kimdonghyun <ssk0080@gmail.com>
2023-09-29 23:49:54 +0900

    백준 b5 팩토리얼

---
+++ java_algorithm/algorithm/src/main/java/org/example/baekjoon/bronze/팩토리얼.java
@@ -0,0 +1,35 @@
+package org.example.baekjoon.bronze;
+
+import java.io.*;
+
+public class 팩토리얼 {
+
+    static int result = 1;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("src/main/java/org/example/baekjoon/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int input = Integer.parseInt(br.readLine());
+
+        dfs(input);
+        sb.append(result);
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
+    }
+
+    public static void dfs(int number) {
+        if (number < 1) {
+            return;
+        }
+
+        result = result * number;
+        dfs(number - 1);
+    }
+
+}
