d285c1bf4f7d40fb963e4e2b7cc5e5e8d6b080f7
kimdonghyun <ssk0080@gmail.com>
2023-10-02 20:55:47 +0900

    main

--- java_algorithm/algorithm/src/main/java/org/example/baekjoon/Main.java
+++ java_algorithm/algorithm/src/main/java/org/example/baekjoon/Main.java
@@ -2,10 +2,19 @@ package org.example.baekjoon;
 
 import java.io.*;
 
-public class MainExam {
+public class Main {
+
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("src/main/java/org/example/baekjoon/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+        // StringTokenizer st = new StringTokenizer();
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+
     }
+
 }
